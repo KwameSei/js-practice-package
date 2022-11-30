0fa64b23e89d9c62cb35d022cd2e48dc59d123bd
KwameSei <nathanielosei92@gmail.com>
2022-11-30 19:39:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// This is our entry point
+// It imports all others modules created
+//
+
+const index = require('./practice');
+
+module.exports = index;
---
+++ practice.js
@@ -0,0 +1,7 @@
+// Creating anonymous function
+const practice = () => {
+	console.log('This is just a basic skeleton package with essential parts');
+}
+
+// Exporting the function to be used by other files
+module.exports = practice;
